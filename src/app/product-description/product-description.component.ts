1daab76c26e5a4f186f8898d7e0c43a265ad7a21
Michal Janek <michal.janek@gmail.com>
2018-03-21 12:54:49 +0100

    fixed it approve it damnit

--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -7,11 +7,11 @@ import { ProductService } from '../product.service';
   styleUrls: ['./product-description.component.css']
 })
 export class ProductDescriptionComponent implements OnInit {
-  albumInfo: any;
+  albumInfo;
   constructor(private _productService: ProductService) { }
 
   ngOnInit() {
-    this._productService.getAlbum(1).subscribe(albumInfo => this.albumInfo = albumInfo);
+    this._productService.getAlbum(1).subscribe(response => this.albumInfo = response);
   }
 
 }
